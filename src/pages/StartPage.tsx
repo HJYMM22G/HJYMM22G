0a34096d3738c8e05ac33c1ae4127312150d8df9
Hampus Isebring <hampus.isebring@gmail.com>
2023-08-30 13:36:54 +0200

    add hero section to landing page

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,8 +1,10 @@
+import { Hero } from '../components/Hero';
 import { TeamSection } from '../components/TeamSection';
 
 export function StartPage() {
   return (
     <>
+      <Hero />
       <TeamSection />
     </>
   );
