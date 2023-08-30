af8e7d0a7e62be9735671560c6ccf14332ef4914
Hampus Isebring <hampus.isebring@gmail.com>
2023-08-30 11:07:56 +0200

    add styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,67 @@
-import { Box, Container, Text, Title } from '@mantine/core';
+import { Box, Container, Text, Title, createStyles, rem } from '@mantine/core';
 import heroimg from '../assets/imgs/hero.png';
 
+const useStyles = createStyles((theme) => ({
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    [theme.fn.smallerThan('md')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  image: {
+    [theme.fn.smallerThan('md')]: {
+      display: 'none',
+    },
+  },
+
+  content: {
+    paddingTop: `calc(${theme.spacing.xl} * 2)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 2)`,
+    marginRight: `calc(${theme.spacing.xl} * 3)`,
+
+    [theme.fn.smallerThan('md')]: {
+      marginRight: 0,
+    },
+  },
+
+  title: {
+    color: theme.black,
+    fontWeight: 700,
+    lineHeight: 1.05,
+    maxWidth: rem(500),
+    fontSize: rem(44),
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+      fontSize: rem(34),
+      lineHeight: 1.15,
+    },
+  },
+
+  description: {
+    color: theme.black,
+    opacity: 0.9,
+    maxWidth: rem(500),
+    fontSize: '14px',
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+    },
+  },
+}));
+
 export function Hero() {
+  const { classes } = useStyles();
   return (
     <Box>
       <Container>
-        <Box>
-          <Box>
-            <Title ta="center">
+        <Box className={classes.inner}>
+          <Box className={classes.content}>
+            <Title ta="center" className={classes.title}>
               Let us{' '}
               <Text component="span" inherit color="#BAB653">
                 help{' '}
@@ -17,7 +71,7 @@ export function Hero() {
               company grow
             </Title>
           </Box>
-          <Box>
+          <Box className={classes.image}>
             <img
               height="500"
               width="500"
