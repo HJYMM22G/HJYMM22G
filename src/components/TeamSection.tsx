9bd4b4f6ada98f9ef8ab52f3bc9b5817a0f92080
Moa Hedendahl <moahedendahl@gmail.com>
2023-08-30 10:29:41 +0200

    designed and made teamsection responsive

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -1,6 +1,50 @@
+import { Col, Container, Grid, Text, Title } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import { staffData } from '../data/staffData';
+
 export function TeamSection() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <Container size="xl">
+      <Container sx={{ display: 'flex', padding: '0', margin: '0', gap: 2 }}>
+        <img src="../src/assets/pin.png" alt="" style={{ height: '1.4rem' }} />
+        <Text sx={{ marginLeft: '0.6rem' }}>Who we are</Text>
+      </Container>
+      <Title order={1}>Empowering your digital future.</Title>
+      <Grid
+        gutter="sm"
+        sx={{
+          display: 'flex',
+          width: '100%',
+          marginTop: '1.5rem',
+          justifyContent: 'center',
+        }}
+      >
+        {staffData.map((person) => (
+          <Col span={12} sm={5} lg={2.4} key={person.id}>
+            <Link
+              to={`/about/${person.name.toLowerCase()}`}
+              style={{ textDecoration: 'none', color: '#2C2E33' }}
+            >
+              <Container style={{ textAlign: 'center' }}>
+                <img
+                  src={person.image}
+                  alt=""
+                  style={{
+                    border: `${person.color} 6px solid`,
+                    borderRadius: '100rem',
+                    height: '9rem',
+                    width: '9rem',
+                    objectFit: 'cover',
+                  }}
+                />
+                <Title order={4}>{person.fullName}</Title>
+                <Text size="sm">{person.role}</Text>
+                <Text>{person.description}</Text>
+              </Container>
+            </Link>
+          </Col>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
