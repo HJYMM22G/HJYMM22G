f4e63c62d44549bae499ccc69d1d41e41b20d6c0
Martina Kreivi <martinakreivi@gmail.com>
2023-08-30 12:48:45 +0200

    added grid

--- src/pages/StaffPage.tsx
+++ src/pages/StaffPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Text } from "@mantine/core";
+import { Col, Container, Grid, Text } from "@mantine/core";
 import { IconPinnedFilled } from "@tabler/icons-react";
 import { useParams } from "react-router-dom";
 import { staffData } from "../data/staffData";
@@ -24,29 +24,70 @@ export function StaffPage() {
       ) : (
         <div>Personen finns tyvärr inte.</div>
       )}
-      <Container sx={{ display: "flex", padding: "0", margin: "0", gap: 2 }}>
-        <img
-          src={person.image}
-          alt=""
-          style={{
-            border: `${person.color} 6px solid`,
-            borderRadius: "100rem",
-            height: "9rem",
-            width: "9rem",
-            objectFit: "cover",
-          }}
-        />
-      </Container>
-      <Container sx={{ display: "flex", padding: "0", margin: "0", gap: 2 }}>
-        <Text>
-          Meet {person.name}, a passionate software engineer at devSTUDIO. With
-          a knack for elegant solutions, {person.name} thrives on transforming
-          complex problems into streamlined code. Their collaborative nature and
-          innovative thinking make them an invaluable asset to the team,
-          consistently delivering high-quality projects that push the boundaries
-          of technology.
-        </Text>
-      </Container>
+
+      <Grid gutter="md" mt="xl">
+        <Col span={6}>
+          <div
+            style={{
+              border: `${person.color} 6px solid`,
+              borderRadius: "100rem",
+              height: "9rem",
+              width: "9rem",
+              objectFit: "cover",
+            }}
+          >
+            <img
+              src={person.image}
+              alt=""
+              style={{
+                height: "100%",
+                width: "100%",
+                objectFit: "cover",
+                borderRadius: "100rem",
+              }}
+            />
+          </div>
+        </Col>
+        <Col span={6}>
+          <Text>
+            Meet {person.name}, a passionate software engineer at devSTUDIO.
+            With a knack for elegant solutions, {person.name} thrives on
+            transforming complex problems into streamlined code. Their
+            collaborative nature and innovative thinking make them an invaluable
+            asset to the team, consistently delivering high-quality projects
+            that push the boundaries of technology.
+          </Text>
+        </Col>
+      </Grid>
+
+      <Grid gutter="md" mt="xl">
+        <Col span={6}>
+          <div
+            style={{
+              border: `${person.color} 6px solid`,
+              borderRadius: "100rem",
+              height: "9rem",
+              width: "9rem",
+              objectFit: "cover",
+            }}
+          >
+            <img
+              src={person.image}
+              alt=""
+              style={{ height: "100%", width: "100%", objectFit: "cover" }}
+            />
+          </div>
+        </Col>
+        <Col span={6}>
+          <Text size="lg">About {person.fullName}</Text>
+          <Text>
+            With a flair for front-end development and user experience design,{" "}
+            {person.name}'s expertise in HTML, CSS, and JavaScript shines
+            through in every project, creating visually stunning and
+            user-friendly applications that push technological boundaries.
+          </Text>
+        </Col>
+      </Grid>
     </Container>
   );
 }
