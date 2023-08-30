f6be6a371cca155d846c439f75e2a77a65c332a4
ye-mou <git.ymo22@gmail.com>
2023-08-30 11:53:04 +0200

    Added som styling to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,39 @@
-export function Header() {
-    return (
-        <>
-        </>
-    )
+import { Container, Text } from "@mantine/core";
+
+export default function Header() {
+  return (
+    <Container
+      style={{
+        backgroundColor: "beige",
+        padding: "1rem",
+        borderBottom: "1px solid #ccc",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ul
+        style={{
+          listStyle: "none",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          padding: 0,
+          margin: 0,
+          width: "100%",
+        }}
+      >
+        <li>
+          <Text size="xl">Home</Text>
+        </li>
+        <li>
+          <Text size="xl">Dashboard</Text>
+        </li>
+        <li>
+          <Text size="xl">Profile</Text>
+        </li>
+      </ul>
+    </Container>
+  );
 }
