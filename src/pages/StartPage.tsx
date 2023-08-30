966fee94dc6e2be43fd9dffcbde0e17b58b6d85f
Moa Hedendahl <moahedendahl@gmail.com>
2023-08-30 11:31:10 +0200

    add servicesection

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,9 +1,11 @@
+import { ServicesSection } from '../components/ServicesSection';
 import { TeamSection } from '../components/TeamSection';
 
 export function StartPage() {
   return (
     <>
       <TeamSection />
+      <ServicesSection />
     </>
   );
 }
