12b5bef0228f0fd4e1a8accc959942861cdbf901
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-08-30 19:29:01 +0200

    basic styling

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,6 +1,64 @@
+import { Box, Button, Container, Select, TextInput, Textarea, createStyles } from "@mantine/core";
+import { IconChevronDown } from "@tabler/icons-react";
+
+const useStyles = createStyles((theme) => ({
+  firstrow: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: '15px',
+    gap: "5rem"
+  },
+  firstrowinput: {
+    flex: 1,
+    backgroundColor: 'transparent'
+  },
+}))
+
+const myStyles = {
+  input: {
+    backgroundColor: 'transparent',
+    borderColor: 'black',
+    boxShadow:  'none',
+  },
+  dropdown: {
+    backgroundColor: '#FBF9F3',
+  }
+};
+
 export function ContactForm() {
-    return (
-        <>
-        </>
-    )
+  const { classes } = useStyles();
+
+  return (
+    <Box>
+      <Container size="md">
+        <Box className={classes.firstrow}>
+          <Box className={classes.firstrowinput}>
+            <TextInput required label="Name" styles={myStyles}/>
+          </Box>
+          <Box className={classes.firstrowinput}>
+            <TextInput required label="Email" styles={myStyles}/>
+          </Box>
+        </Box>
+
+        <Box style={{ marginBottom: '15px' }}>
+          <Select
+            label="Person"
+            required
+            rightSection={<IconChevronDown size="1rem" />}
+            rightSectionWidth={30}
+            styles={myStyles}
+            data={['Person 1', 'Person 2', 'Person 3', 'Person 4', 'Person 5']}
+          />
+        </Box>
+
+        <Box style={{ marginBottom: '15px' }}>
+          <Textarea required label="Message" styles={myStyles}/>
+        </Box>
+
+        <Box style={{ textAlign: 'center' }}>
+          <Button>Send message</Button>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,4 +1,5 @@
 import BrandBanner from '../components/BrandBanner';
+import { ContactForm } from '../components/ContactForm';
 import { Hero } from '../components/Hero';
 import { ServicesSection } from '../components/ServicesSection';
 import { TeamSection } from '../components/TeamSection';
@@ -9,6 +10,7 @@ export function StartPage() {
       <Hero />
       <TeamSection />
       <ServicesSection />
+      <ContactForm />
       <BrandBanner />
     </>
   );
