e55e7c612ec8c50c8937962e621ae8868b0ee38b
Martina Kreivi <martinakreivi@gmail.com>
2023-08-31 08:17:46 +0200

    New setup

--- src/pages/StaffPage.tsx
+++ src/pages/StaffPage.tsx
@@ -10,77 +10,61 @@ export function StaffPage() {
     : null;
 
   return (
-    <Container size="xl">
-      <Container sx={{ display: "flex", padding: "0", margin: "0", gap: 2 }}>
-        <IconPinnedFilled />
-        <Text sx={{ marginLeft: "0.6rem" }}>Who we are</Text>
-      </Container>
-
-      <Grid gutter="md" mt="xl">
-        <Col span={6}>
-          {person ? (
-            <div>
-              <h1>{person.name}</h1>
-              <p>Role: {person.role}</p>
-              <p>Bio: {person.bio}</p>
-            </div>
-          ) : (
-            <div>Personen finns tyvärr inte.</div>
-          )}
-        </Col>
-        <Col span={6}>
-          <div
-            style={{
-              border: `${person ? person.color : "gray"} 6px solid`,
-              borderRadius: "100rem",
-              height: "9rem",
-              width: "9rem",
-              objectFit: "cover",
-            }}
-          >
-            <img
-              src={person ? person.image : ".../assets/placeholder.png"}
-              alt=""
-              style={{
-                height: "100%",
-                width: "100%",
-                objectFit: "cover",
-                borderRadius: "100rem",
-              }}
-            />
+    <Grid gutter="md">
+      <Col span={12}>
+        {" "}
+        <Container
+          sx={{ display: "flex", padding: "0", margin: "2rem", gap: 2 }}
+        >
+          <IconPinnedFilled />
+          <Text sx={{ marginLeft: "0.6rem" }}>Who we are</Text>
+        </Container>
+      </Col>
+      <Col span={6}>
+        {person ? (
+          <div>
+            <h1>{person.name}</h1>
+            <p>Role: {person.role}</p>
+            <p>Bio: {person.bio}</p>
           </div>
-        </Col>
-      </Grid>
-
-      <Grid gutter="md" mt="xl">
-        <Col span={6}>
-          <div
+        ) : (
+          <div>Personen finns tyvärr inte.</div>
+        )}
+      </Col>
+      <Col span={6}>
+        {" "}
+        <div
+          style={{
+            border: `${person ? person.color : "gray"} 6px solid`,
+            borderRadius: "100rem",
+            height: "12rem",
+            width: "12rem",
+            objectFit: "cover",
+          }}
+        >
+          <img
+            src={person ? person.image : ".../assets/placeholder.png"}
+            alt=""
             style={{
-              border: `${person ? person.color : "gray"} 6px solid`,
-              borderRadius: "100rem",
-              height: "9rem",
-              width: "9rem",
+              height: "100%",
+              width: "100%",
               objectFit: "cover",
+              borderRadius: "100rem",
             }}
-          >
-            <img
-              src=".../assets/VectorIllustration1.png"
-              alt=""
-              style={{ height: "100%", width: "100%", objectFit: "cover" }}
-            />
-          </div>
-        </Col>
-        <Col span={6}>
-          <Text size="lg">About {person ? person.fullName : "Name"}</Text>
-          <Text>
-            With a flair for front-end development and user experience design,{" "}
-            {person ? person.name : "they"}'s expertise in HTML, CSS, and
-            JavaScript shines through in every project, creating visually
-            stunning and user-friendly applications that push technological
-            boundaries.
-          </Text>
-        </Col>
-      </Grid>
-    </Container>
+          />
+        </div>
+      </Col>
+      <Col span={6}>
+        <Text size="lg">About {person ? person.fullName : "Name"}</Text>
+        <Text>
+          With a flair for front-end development and user experience design,{" "}
+          {person ? person.name : "they"}'s expertise in HTML, CSS, and
+          JavaScript shines through in every project, creating visually stunning
+          and user-friendly applications that push technological boundaries.
+        </Text>
+      </Col>
+      <Col span={6}>Vector bild</Col>
+      <Col span={6}>Kompetenser</Col>
+    </Grid>
   );
 }
