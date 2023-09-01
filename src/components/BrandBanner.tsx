94d2a7f6c0080256ae7fc369431898df0a80a83d
ye-mou <git.ymo22@gmail.com>
2023-09-01 13:04:59 +0200

    GentsHat in BrandBanner

--- src/components/BrandBanner.tsx
+++ src/components/BrandBanner.tsx
@@ -1,52 +1,57 @@
-import { Container, Text } from '@mantine/core';
-import { IconPinnedFilled } from '@tabler/icons-react';
-import Marquee from 'react-fast-marquee';
-import Boop from './Boop';
+import { Container, Text } from "@mantine/core";
+import { IconPinnedFilled } from "@tabler/icons-react";
+import Marquee from "react-fast-marquee";
+import Boop from "./Boop";
 
 function BrandBanner() {
   return (
     <Container sx={{}}>
-      <Container sx={{ display: 'flex', padding: '0', margin: '1rem 0' }}>
+      <Container sx={{ display: "flex", padding: "0", margin: "1rem 0" }}>
         <Boop rotation={10} timing={150}>
           <IconPinnedFilled />
         </Boop>
-        <Text sx={{ marginLeft: '0.6rem' }}>Our Clients</Text>
+        <Text sx={{ marginLeft: "0.6rem" }}>Our Clients</Text>
       </Container>
       <Marquee
         autoFill={true}
         gradient={true}
         gradientColor={[251, 249, 243]}
-        style={{ height: 'auto' }}
+        style={{ height: "auto" }}
       >
         <img
           src="../src/assets/logos/candycakes.png"
           alt=""
-          style={{ height: '3rem', marginRight: '3rem' }}
+          style={{ height: "3rem", marginRight: "3rem" }}
         />
         <img
           src="../src/assets/logos/chatclub.png"
           alt=""
-          style={{ height: '3rem', marginRight: '3rem' }}
+          style={{ height: "3rem", marginRight: "3rem" }}
         />
         <img
           src="../src/assets/logos/chatropolis.png"
           alt=""
-          style={{ height: '3rem', marginRight: '3rem' }}
+          style={{ height: "3rem", marginRight: "3rem" }}
         />
         <img
           src="../src/assets/logos/share.png"
           alt=""
-          style={{ height: '3rem', marginRight: '3rem' }}
+          style={{ height: "3rem", marginRight: "3rem" }}
         />
         <img
           src="../src/assets/logos/takenbake.png"
           alt=""
-          style={{ height: '3rem', marginRight: '3rem' }}
+          style={{ height: "3rem", marginRight: "3rem" }}
         />
         <img
           src="../src/assets/logos/sunpop.png"
           alt=""
-          style={{ height: '3rem', marginRight: '3rem' }}
+          style={{ height: "3rem", marginRight: "3rem" }}
+        />
+        <img
+          src="../src/assets/logos/GentsHat.png"
+          alt=""
+          style={{ height: "3rem", marginRight: "3rem" }}
         />
       </Marquee>
     </Container>
