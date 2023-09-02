76ffc1157df1b6148ab0aa24c3a384f30211e160
Hampus Isebring <hampus.isebring@gmail.com>
2023-09-02 12:36:25 +0200

    add new header + navbarData

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,55 +1,175 @@
-import { Anchor, Container, createStyles, rem } from "@mantine/core";
+import {
+  Burger,
+  Container,
+  Group,
+  Header,
+  Paper,
+  Transition,
+  createStyles,
+  rem,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { NavHashLink } from 'react-router-hash-link';
+import logo from '../assets/imgs/logo.png';
+
+const headerHeight = rem(65);
 
 const useStyles = createStyles((theme) => ({
-  headerContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: `0 ${theme.spacing.md}`,
+  root: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+    borderBottom: 'none',
+    transition: 'transform 0.3s ease-in-out',
+    background: '#fbf9f3',
   },
-  logo: {
-    width: rem(60),
+
+  dropdown: {
+    position: 'absolute',
+    top: headerHeight,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    background: '#fbf9f3',
+
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
   },
-  linkContainer: {
-    marginLeft: "auto",
-    marginRight: 0,
+
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
   },
+
+  links: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
   link: {
-    color: theme.black,
-    textDecoration: "none",
-    fontSize: rem(16),
+    display: 'block',
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: 'none',
+    color: '#000000',
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 600,
+
+    '&:hover': {
+      textDecoration: 'underline',
+      textDecorationColor: '#BAB653',
+    },
+
+    [theme.fn.smallerThan('sm')]: {
+      borderRadius: 0,
+      padding: theme.spacing.md,
+    },
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      textDecoration: 'underline',
+      textDecorationColor: '#BAB653',
+    },
   },
 }));
 
-const Header = () => {
-  const { classes } = useStyles();
+export interface NavbarProps {
+  links: { link: string; label: string }[];
+}
+
+export function Navbar({ links }: NavbarProps) {
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const [active, setActive] = useState('string');
+  const { classes, cx } = useStyles();
+
+  function handleReturnClick() {
+    setActive('string');
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  function scrollBackToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  const navigate = useNavigate();
+
+  const handleNavigateClick = (link: string) => {
+    setActive(link);
+    navigate(link);
+    close();
+  };
+
+  const items = links.map((link) => (
+    <NavHashLink
+      key={link.label}
+      to={link.link}
+      className={cx(classes.link, {
+        [classes.linkActive]: active === link.link,
+      })}
+      onClick={() => handleNavigateClick(link.link)}
+    >
+      {link.label}
+    </NavHashLink>
+  ));
 
   return (
-    <Container size="xl">
-      <div className={classes.headerContainer}>
-        <div className={classes.logo}>
-          <img
-            src="../src/assets/logos/devSTUDIO.png"
-            alt="Logo"
-            width="100%"
-          />
-        </div>
-        <div className={classes.linkContainer}>
-          <Container style={{ display: "flex", gap: "1rem" }}>
-            <Anchor className={classes.link} href="/services">
-              Services
-            </Anchor>
-            <Anchor className={classes.link} href="/AboutUs">
-              About Us
-            </Anchor>
-            <Anchor className={classes.link} href="/Contact">
-              Contact
-            </Anchor>
-          </Container>
-        </div>
-      </div>
-    </Container>
-  );
-};
+    <Header height={headerHeight} className={classes.root}>
+      <Container size="md" className={classes.header}>
+        <Group ta="start">
+          <Link to="./" onClick={handleReturnClick}>
+            <img
+              style={{ height: '2.8rem', width: '5rem' }}
+              alt="Logo of Hike and Peak"
+              src={logo}
+            ></img>
+          </Link>
+        </Group>
+        <Group onClick={scrollBackToTop} spacing={5} className={classes.links}>
+          {items}
+        </Group>
 
-export default Header;
+        <Burger
+          color="black"
+          opened={opened}
+          onClick={toggle}
+          className={classes.burger}
+          size="md"
+        />
+
+        <Transition transition="pop-top-right" duration={200} mounted={opened}>
+          {(styles) => (
+            <Paper
+              onClick={scrollBackToTop}
+              className={classes.dropdown}
+              style={styles}
+            >
+              {items}
+            </Paper>
+          )}
+        </Transition>
+      </Container>
+    </Header>
+  );
+}
---
+++ src/data/navbarData.ts
@@ -0,0 +1,7 @@
+import { NavbarProps } from '../components/Header';
+
+export const navData: NavbarProps['links'] = [
+  { link: '/#services', label: 'Services' },
+  { link: '/#find-us', label: 'Find Us' },
+  { link: '/#contact', label: 'Contact' },
+];
