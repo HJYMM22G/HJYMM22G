f3bd69f96ba4c0a07235cda9e9ea9553f8d900eb
ye-mou <git.ymo22@gmail.com>
2023-09-03 14:49:01 +0200

    Burger menu toggle and links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
 import { Anchor, Container, createStyles, rem } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import { useState } from "react";
+import BurgerMenu from "./BurgerMenu";
 
 const useStyles = createStyles((theme) => ({
   headerContainer: {
@@ -7,44 +10,83 @@ const useStyles = createStyles((theme) => ({
     justifyContent: "space-between",
     padding: `0 ${theme.spacing.md}`,
   },
+
   logo: {
+    position: "relative",
     width: rem(60),
+    [theme.fn.smallerThan("sm")]: {
+      width: rem(40),
+    },
   },
+
   linkContainer: {
     marginLeft: "auto",
     marginRight: 0,
+    display: "flex",
+    gap: "1rem",
   },
+
   link: {
-    color: theme.black,
+    color: theme.colors.dark[7],
     textDecoration: "none",
-    fontSize: rem(16),
+    fontSize: rem(18),
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  contentContainer: {
+    marginTop: 0, //!Important dont remove.
+    transition: "margin-top 0.3s ease-in-out",
+  },
+
+  contentContainerOpen: {
+    marginTop: rem(110),
   },
 }));
 
 const Header = () => {
   const { classes } = useStyles();
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const isSmallScreen = useMediaQuery("(max-width: 768px)");
+
+  const handleBurgerToggle = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
-    <Container size="xl">
-      <div className={classes.headerContainer}>
-        <div className={classes.logo}>
-          <img src="../src/assets/imgs/logo.png" alt="Logo" width="100%" />
+    <>
+      <Container size="xl">
+        <div className={classes.headerContainer}>
+          <div className={classes.logo}>
+            <img src="../src/assets/imgs/logo.png" alt="Logo" width="100%" />
+          </div>
+          <div className={classes.linkContainer}>
+            {isSmallScreen ? (
+              <BurgerMenu onToggle={handleBurgerToggle} />
+            ) : (
+              <>
+                <Anchor className={classes.link} href="/services">
+                  Services
+                </Anchor>
+                <Anchor className={classes.link} href="/AboutUs">
+                  About Us
+                </Anchor>
+                <Anchor className={classes.link} href="/Contact">
+                  Contact
+                </Anchor>
+              </>
+            )}
+          </div>
         </div>
-        <div className={classes.linkContainer}>
-          <Container style={{ display: "flex", gap: "1rem" }}>
-            <Anchor className={classes.link} href="/services">
-              Services
-            </Anchor>
-            <Anchor className={classes.link} href="/AboutUs">
-              About Us
-            </Anchor>
-            <Anchor className={classes.link} href="/Contact">
-              Contact
-            </Anchor>
-          </Container>
-        </div>
-      </div>
-    </Container>
+      </Container>
+
+      <div
+        className={`${classes.contentContainer} ${
+          isMobileMenuOpen ? classes.contentContainerOpen : ""
+        }`}
+      ></div>
+    </>
   );
 };
 
