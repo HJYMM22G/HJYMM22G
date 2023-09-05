9352bdc1d7f05bd65d32e2efb26c41a13f2fd3f4
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-09-05 12:43:12 +0200

    team + service

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -1,17 +1,18 @@
-import { Container, Text, Title } from '@mantine/core';
-import { IconPinnedFilled } from '@tabler/icons-react';
+import {Container, Text, Title} from '@mantine/core';
+import {IconPinnedFilled} from '@tabler/icons-react';
+import {useTranslation} from 'react-i18next';
 import Boop from './Boop';
 
 export function ServicesSection() {
+  const {t} = useTranslation();
   return (
     <Container
-      size="xl"
+      size='xl'
       sx={{
         display: 'flex',
         marginTop: '4rem',
         marginBottom: '4rem',
-      }}
-    >
+      }}>
       <Container
         sx={{
           display: 'flex',
@@ -20,12 +21,11 @@ export function ServicesSection() {
           '@media (max-width: 800px)': {
             display: 'none',
           },
-        }}
-      >
+        }}>
         <img
-          src="../src/assets/services.png"
-          alt=""
-          style={{ width: '100%', height: '100%' }}
+          src='../src/assets/services.png'
+          alt=''
+          style={{width: '100%', height: '100%'}}
         />
       </Container>
       <Container
@@ -39,27 +39,36 @@ export function ServicesSection() {
           '@media (max-width: 800px)': {
             paddingLeft: '0',
           },
-        }}
-      >
-        <Container sx={{ display: 'flex', padding: '0', margin: '0' }}>
-          <Boop rotation={10} timing={150}>
+        }}>
+        <Container
+          sx={{display: 'flex', padding: '0', margin: '0'}}>
+          <Boop
+            rotation={10}
+            timing={150}>
             <IconPinnedFilled />
           </Boop>
-          <Text sx={{ marginLeft: '0.6rem' }}>Our Services</Text>
+          <Text sx={{marginLeft: '0.6rem'}}>
+            {t('servicesection.ourServices')}
+          </Text>
         </Container>
-        <Title order={1} sx={{ paddingBottom: '0.5rem' }}>
-          Our Area of Expertise
+        <Title
+          order={1}
+          sx={{paddingBottom: '0.5rem'}}>
+          {t('servicesection.areaOfExpertise')}
         </Title>
-        <Text sx={{ paddingBottom: '1rem' }}>
-          From initial idea to finished product, we will bring your vision to
-          life.
+        <Text sx={{paddingBottom: '1rem'}}>
+          {t('servicesection.fromIdeaToProduct')}
         </Text>
-        <Title order={3}>Front End Development</Title>
-        <Text sx={{ paddingBottom: '1rem' }}>
+        <Title order={3}>
+          {t('servicesection.frontEndTitle')}
+        </Title>
+        <Text sx={{paddingBottom: '1rem'}}>
           HTML, CSS and JavaScript. React and Angular.
         </Text>
-        <Title order={3}>Back End Development</Title>
-        <Text sx={{ paddingBottom: '1rem' }}>
+        <Title order={3}>
+          {t('servicesection.backEndTitle')}
+        </Title>
+        <Text sx={{paddingBottom: '1rem'}}>
           Node JS, MongoDB, Express JS.
         </Text>
         <Title order={3}>Digital Design</Title>
--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -1,48 +1,71 @@
-import { Col, Container, Grid, Text, Title } from '@mantine/core';
-import { IconPinnedFilled } from '@tabler/icons-react';
-import { Link } from 'react-router-dom';
-import { staffData } from '../data/staffData';
+import {
+  Col,
+  Container,
+  Grid,
+  Text,
+  Title,
+} from '@mantine/core';
+import {IconPinnedFilled} from '@tabler/icons-react';
+import {useTranslation} from 'react-i18next';
+import {Link} from 'react-router-dom';
+import {staffData} from '../data/staffData';
 import Boop from './Boop';
 
 export function TeamSection() {
+  const {t} = useTranslation();
   const handleMouseEnter = (event: {
-    currentTarget: { style: { transform: string } };
+    currentTarget: {style: {transform: string}};
   }) => {
     event.currentTarget.style.transform = 'scale(1.1)';
   };
 
   const handleMouseLeave = (event: {
-    currentTarget: { style: { transform: string } };
+    currentTarget: {style: {transform: string}};
   }) => {
     event.currentTarget.style.transform = 'scale(1)';
   };
   return (
-    <Container size="xl">
-      <Container sx={{ display: 'flex', padding: '0', margin: '0', gap: 2 }}>
-        <Boop rotation={10} timing={150}>
+    <Container size='xl'>
+      <Container
+        sx={{
+          display: 'flex',
+          padding: '0',
+          margin: '0',
+          gap: 2,
+        }}>
+        <Boop
+          rotation={10}
+          timing={150}>
           <IconPinnedFilled />
         </Boop>
-        <Text sx={{ marginLeft: '0.6rem' }}>Who we are</Text>
+        <Text sx={{marginLeft: '0.6rem'}}>
+          {t('teamsection.whoWeAre')}
+        </Text>
       </Container>
-      <Title order={1}>Empowering your digital future.</Title>
+      <Title order={1}>{t('teamsection.title')}</Title>
       <Grid
-        gutter="sm"
+        gutter='sm'
         sx={{
           display: 'flex',
           width: '100%',
           marginTop: '1.5rem',
           justifyContent: 'center',
-        }}
-      >
+        }}>
         {staffData.map((person) => (
-          <Col span={12} sm={5} lg={2.4} key={person.id}>
+          <Col
+            span={12}
+            sm={5}
+            lg={2.4}
+            key={person.id}>
             <Link
               to={`/about/${person.name.toLowerCase()}`}
               onClick={() => {
                 window.scroll(0, 0);
               }}
-              style={{ textDecoration: 'none', color: '#2C2E33' }}
-            >
+              style={{
+                textDecoration: 'none',
+                color: '#2C2E33',
+              }}>
               <Container
                 style={{
                   textAlign: 'center',
@@ -50,11 +73,10 @@ export function TeamSection() {
                   transition: 'transform 0.3s ease',
                 }}
                 onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-              >
+                onMouseLeave={handleMouseLeave}>
                 <img
                   src={person.image}
-                  alt=""
+                  alt=''
                   style={{
                     border: `${person.color} 6px solid`,
                     borderRadius: '100rem',
@@ -64,7 +86,7 @@ export function TeamSection() {
                   }}
                 />
                 <Title order={4}>{person.fullName}</Title>
-                <Text size="sm">{person.role}</Text>
+                <Text size='sm'>{person.role}</Text>
                 <Text>{person.bio}</Text>
               </Container>
             </Link>
