ee45d5da1feabf32e7dab52518b38242fa8436fb
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-09-05 21:18:18 +0200

    staffpage

--- src/pages/StaffPage.tsx
+++ src/pages/StaffPage.tsx
@@ -1,4 +1,11 @@
-import { Col, Container, Grid, Image, Text, Title } from '@mantine/core';
+import {
+  Col,
+  Container,
+  Grid,
+  Image,
+  Text,
+  Title,
+} from '@mantine/core';
 import {
   IconBrandCss3,
   IconBrandFigma,
@@ -10,18 +17,22 @@ import {
   IconBrandTypescript,
   IconPinnedFilled,
 } from '@tabler/icons-react';
-import { useParams } from 'react-router-dom';
-import { staffData } from '../data/staffData';
+import {useParams} from 'react-router-dom';
+import {useTranslatedStaffData} from '../hooks/useTranslatedStaffData';
 
 export function StaffPage() {
-  const { staffname } = useParams<{ staffname: string }>();
+  const {staffname} = useParams<{staffname: string}>();
+  const translatedStaffData = useTranslatedStaffData();
   const person = staffname
-    ? staffData.find((i) => i.name.toLowerCase() === staffname.toLowerCase())
+    ? translatedStaffData.find(
+        (i) =>
+          i.name.toLowerCase() === staffname.toLowerCase(),
+      )
     : null;
 
   return (
     <Container>
-      <Grid gutter="md">
+      <Grid gutter='md'>
         <Col
           span={12}
           sm={12}
@@ -35,8 +46,7 @@ export function StaffPage() {
             '@media (min-width: 994px)': {
               display: 'none',
             },
-          }}
-        >
+          }}>
           <Container
             sx={{
               height: '15rem',
@@ -46,13 +56,18 @@ export function StaffPage() {
                 height: '10rem',
                 width: '10rem',
               },
-            }}
-          >
+            }}>
             <img
-              src={person ? person.image : '.../assets/placeholder.png'}
+              src={
+                person
+                  ? person.image
+                  : '.../assets/placeholder.png'
+              }
               alt={person?.fullName || 'Name'}
               style={{
-                border: `${person ? person.color : 'gray'} 6px solid`,
+                border: `${
+                  person ? person.color : 'gray'
+                } 6px solid`,
                 height: '100%',
                 width: '100%',
                 objectFit: 'cover',
@@ -65,35 +80,51 @@ export function StaffPage() {
           </Container>
         </Col>
 
-        <Col span={12} sm={12} md={6}>
+        <Col
+          span={12}
+          sm={12}
+          md={6}>
           {' '}
           <Container
-            sx={{ display: 'flex', padding: '0', marginTop: '3rem', gap: 2 }}
-          >
+            sx={{
+              display: 'flex',
+              padding: '0',
+              marginTop: '3rem',
+              gap: 2,
+            }}>
             <IconPinnedFilled />
-            <Text sx={{ marginLeft: '0.6rem' }}>Who we are</Text>
+            <Text sx={{marginLeft: '0.6rem'}}>
+              Who we are
+            </Text>
           </Container>
           <Title>{person?.fullName}</Title>
-          <Text fz="sm" fs="italic">
+          <Text
+            fz='sm'
+            fs='italic'>
             {person?.role}
           </Text>
           <Text mt={'1rem'}>{person?.bio}</Text>
-          <Title size="sm" mt={'2rem'}>
+          <Title
+            size='sm'
+            mt={'2rem'}>
             Contact me!
           </Title>
           <Text mt={'0.2rem'}>{person?.mail}</Text>
-          <Title order={2} sx={{ marginTop: '1rem' }}>
+          <Title
+            order={2}
+            sx={{marginTop: '1rem'}}>
             About {person ? person.name : 'Name'}
           </Title>
           <Text mt={'0.2rem'}>{person?.description}</Text>
-          <Title order={2} sx={{ marginTop: '1rem' }}>
+          <Title
+            order={2}
+            sx={{marginTop: '1rem'}}>
             My Expertises
           </Title>
           <div
             style={{
               marginTop: '1rem',
-            }}
-          >
+            }}>
             <IconBrandHtml5
               style={{
                 color: '#FF0000',
@@ -175,12 +206,13 @@ export function StaffPage() {
             '@media (max-width: 994px)': {
               display: 'none',
             },
-          }}
-        >
+          }}>
           {' '}
           <div
             style={{
-              border: `${person ? person.color : 'gray'} 6px solid`,
+              border: `${
+                person ? person.color : 'gray'
+              } 6px solid`,
               borderRadius: '100rem',
               height: '15rem',
               width: '15rem',
@@ -188,10 +220,13 @@ export function StaffPage() {
               display: 'flex',
               justifyContent: 'center',
               alignContent: 'center',
-            }}
-          >
+            }}>
             <img
-              src={person ? person.image : '.../assets/placeholder.png'}
+              src={
+                person
+                  ? person.image
+                  : '.../assets/placeholder.png'
+              }
               alt={person?.fullName || 'Name'}
               style={{
                 height: '100%',
@@ -206,10 +241,10 @@ export function StaffPage() {
           </div>
           <Image
             maw={300}
-            mx="auto"
-            radius="md"
+            mx='auto'
+            radius='md'
             src={person?.funImage}
-            alt="Vector staff"
+            alt='Vector staff'
           />
         </Col>
         <Col
@@ -224,14 +259,13 @@ export function StaffPage() {
             '@media (min-width: 994px)': {
               display: 'none',
             },
-          }}
-        >
+          }}>
           <Image
             maw={300}
-            mx="auto"
-            radius="md"
-            src="../src/assets/staff.png"
-            alt="Vector staff"
+            mx='auto'
+            radius='md'
+            src='../src/assets/staff.png'
+            alt='Vector staff'
           />
         </Col>
       </Grid>
