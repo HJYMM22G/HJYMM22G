4f8e35ab512d9ba4606c28eedbb58d677634db2e
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-09-09 15:39:29 +0200

    staff img

--- src/pages/StaffPage.tsx
+++ src/pages/StaffPage.tsx
@@ -1,4 +1,11 @@
-import { Col, Container, Grid, Image, Text, Title } from "@mantine/core";
+import {
+  Col,
+  Container,
+  Grid,
+  Image,
+  Text,
+  Title,
+} from '@mantine/core';
 import {
   IconBrandCss3,
   IconBrandFigma,
@@ -9,19 +16,20 @@ import {
   IconBrandReact,
   IconBrandTypescript,
   IconPinnedFilled,
-} from "@tabler/icons-react";
-import { Helmet } from "react-helmet-async";
-import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
-import { useTranslatedStaffData } from "../hooks/useTranslatedStaffData";
+} from '@tabler/icons-react';
+import {Helmet} from 'react-helmet-async';
+import {useTranslation} from 'react-i18next';
+import {useParams} from 'react-router-dom';
+import {useTranslatedStaffData} from '../hooks/useTranslatedStaffData';
 
 export function StaffPage() {
-  const { staffname } = useParams<{ staffname: string }>();
-  const { t } = useTranslation();
+  const {staffname} = useParams<{staffname: string}>();
+  const {t} = useTranslation();
   const translatedStaffData = useTranslatedStaffData();
   const person = staffname
     ? translatedStaffData.find(
-        (i) => i.name.toLowerCase() === staffname.toLowerCase()
+        (i) =>
+          i.name.toLowerCase() === staffname.toLowerCase(),
       )
     : null;
 
@@ -30,176 +38,190 @@ export function StaffPage() {
       <Helmet>
         <title>{person?.fullName} - devStudio</title>
         <meta
-          name="description"
-          content="A website for our development studio"
+          name='description'
+          content='A website for our development studio'
         />
       </Helmet>
-      <Grid gutter="md">
+      <Grid gutter='md'>
         <Col
           span={12}
           sm={12}
           md={6}
           sx={{
-            "@media (max-width: 994px)": {
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "2rem",
+            '@media (max-width: 994px)': {
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '2rem',
             },
-            "@media (min-width: 994px)": {
-              display: "none",
+            '@media (min-width: 994px)': {
+              display: 'none',
             },
-          }}
-        >
+          }}>
           <Container
             sx={{
-              height: "15rem",
-              width: "15rem",
+              height: '15rem',
+              width: '15rem',
 
-              "@media (max-width: 800px)": {
-                height: "10rem",
-                width: "10rem",
+              '@media (max-width: 800px)': {
+                height: '10rem',
+                width: '10rem',
               },
-            }}
-          >
+            }}>
             <img
-              src={person ? person.image : ".../assets/placeholder.png"}
-              alt={person?.fullName || "Name"}
+              src={
+                person
+                  ? person.image
+                  : '.../assets/placeholder.png'
+              }
+              alt={person?.fullName || 'Name'}
               style={{
-                border: `${person ? person.color : "gray"} 6px solid`,
-                height: "100%",
-                width: "100%",
-                objectFit: "cover",
-                borderRadius: "100rem",
-                display: "flex",
-                justifyContent: "center",
-                alignContent: "center",
+                border: `${
+                  person ? person.color : 'gray'
+                } 6px solid`,
+                height: '100%',
+                width: '100%',
+                objectFit: 'cover',
+                borderRadius: '100rem',
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
               }}
             />
           </Container>
         </Col>
 
-        <Col span={12} sm={12} md={6}>
-          {" "}
+        <Col
+          span={12}
+          sm={12}
+          md={6}>
+          {' '}
           <Container
             sx={{
-              display: "flex",
-              padding: "0",
-              marginTop: "4.5rem",
+              display: 'flex',
+              padding: '0',
+              marginTop: '4.5rem',
               gap: 2,
-            }}
-          >
+            }}>
             <IconPinnedFilled />
-            <Text sx={{ marginLeft: "0.6rem" }}>{t("staffpage.whoWeAre")}</Text>
+            <Text sx={{marginLeft: '0.6rem'}}>
+              {t('staffpage.whoWeAre')}
+            </Text>
           </Container>
           <Title>{person?.fullName}</Title>
-          <Text fz="sm" fs="italic">
+          <Text
+            fz='sm'
+            fs='italic'>
             {person?.role}
           </Text>
-          <Text mt={"1rem"}>{person?.bio}</Text>
-          <Title size="sm" mt={"2rem"}>
-            {t("staffpage.contactMe")}
+          <Text mt={'1rem'}>{person?.bio}</Text>
+          <Title
+            size='sm'
+            mt={'2rem'}>
+            {t('staffpage.contactMe')}
           </Title>
-          <Text mt={"0.2rem"}>
-            {" "}
+          <Text mt={'0.2rem'}>
+            {' '}
             <a
               style={{
-                color: "inherit",
-                textDecoration: "none",
+                color: 'inherit',
+                textDecoration: 'none',
               }}
-              href={`mailto:${person?.mail}`}
-            >
+              href={`mailto:${person?.mail}`}>
               {person?.mail}
             </a>
           </Text>
-          <Text mt={"0.2rem"}>
-            {" "}
+          <Text mt={'0.2rem'}>
+            {' '}
             <a
               style={{
-                color: "inherit",
-                textDecoration: "none",
+                color: 'inherit',
+                textDecoration: 'none',
               }}
-              href={`tel:${person?.number}`}
-            >
+              href={`tel:${person?.number}`}>
               {person?.number}
             </a>
           </Text>
-          <Title order={2} sx={{ marginTop: "1rem" }}>
-            {t("staffpage.about")} {person ? person.name : "Name"}
+          <Title
+            order={2}
+            sx={{marginTop: '1rem'}}>
+            {t('staffpage.about')}{' '}
+            {person ? person.name : 'Name'}
           </Title>
-          <Text mt={"0.2rem"}>{person?.description1}</Text>
-          <Text mt={"1rem"}>{person?.description2}</Text>
-          <Text mt={"1rem"}>{person?.description3}</Text>
-          <Title order={2} sx={{ marginTop: "1rem" }}>
-            {t("staffpage.expertise")}
+          <Text mt={'0.2rem'}>{person?.description1}</Text>
+          <Text mt={'1rem'}>{person?.description2}</Text>
+          <Text mt={'1rem'}>{person?.description3}</Text>
+          <Title
+            order={2}
+            sx={{marginTop: '1rem'}}>
+            {t('staffpage.expertise')}
           </Title>
           <div
             style={{
-              marginTop: "1rem",
-            }}
-          >
+              marginTop: '1rem',
+            }}>
             <IconBrandHtml5
               style={{
-                color: "#FF0000",
-                height: "3rem",
-                width: "3rem",
-                margin: "0.25rem",
+                color: '#FF0000',
+                height: '3rem',
+                width: '3rem',
+                margin: '0.25rem',
               }}
             />
             <IconBrandCss3
               style={{
-                color: "#0057FF",
-                height: "3rem",
-                width: "3rem",
-                margin: "0.25rem",
+                color: '#0057FF',
+                height: '3rem',
+                width: '3rem',
+                margin: '0.25rem',
               }}
             />
             <IconBrandJavascript
               style={{
-                color: "#AAD30A",
-                height: "3rem",
-                width: "3rem",
-                margin: "0.25rem",
+                color: '#AAD30A',
+                height: '3rem',
+                width: '3rem',
+                margin: '0.25rem',
               }}
             />
             <IconBrandTypescript
               style={{
-                color: "#0779FE",
-                height: "3rem",
-                width: "3rem",
-                margin: "0.25rem",
+                color: '#0779FE',
+                height: '3rem',
+                width: '3rem',
+                margin: '0.25rem',
               }}
             />
 
             <IconBrandReact
               style={{
-                color: "#61DAFB",
-                height: "3rem",
-                width: "3rem",
-                margin: "0.25rem",
+                color: '#61DAFB',
+                height: '3rem',
+                width: '3rem',
+                margin: '0.25rem',
               }}
             />
             <IconBrandNpm
               style={{
-                color: "#970000",
-                height: "3rem",
-                width: "3rem",
-                margin: "0.25rem",
+                color: '#970000',
+                height: '3rem',
+                width: '3rem',
+                margin: '0.25rem',
               }}
             />
             <IconBrandFigma
               style={{
-                color: "#283BA2",
-                height: "3rem",
-                width: "3rem",
-                margin: "0.25rem",
+                color: '#283BA2',
+                height: '3rem',
+                width: '3rem',
+                margin: '0.25rem',
               }}
             />
             <IconBrandGithub
               style={{
-                color: "black",
-                height: "3rem",
-                width: "3rem",
-                margin: "0.25rem",
+                color: 'black',
+                height: '3rem',
+                width: '3rem',
+                margin: '0.25rem',
               }}
             />
           </div>
@@ -210,49 +232,53 @@ export function StaffPage() {
           sm={12}
           md={6}
           sx={{
-            display: "flex",
+            display: 'flex',
             // justifyContent: 'center',
-            flexDirection: "column",
-            alignItems: "center",
-            marginTop: "4.5rem",
-            "@media (max-width: 994px)": {
-              display: "none",
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '4.5rem',
+            '@media (max-width: 994px)': {
+              display: 'none',
             },
-          }}
-        >
-          {" "}
+          }}>
+          {' '}
           <div
             style={{
-              border: `${person ? person.color : "gray"} 6px solid`,
-              borderRadius: "100rem",
-              height: "15rem",
-              width: "15rem",
+              border: `${
+                person ? person.color : 'gray'
+              } 6px solid`,
+              borderRadius: '100rem',
+              height: '15rem',
+              width: '15rem',
 
-              display: "flex",
-              justifyContent: "center",
-              alignContent: "center",
-            }}
-          >
+              display: 'flex',
+              justifyContent: 'center',
+              alignContent: 'center',
+            }}>
             <img
-              src={person ? person.image : ".../assets/placeholder.png"}
-              alt={person?.fullName || "Name"}
+              src={
+                person
+                  ? person.image
+                  : '.../assets/placeholder.png'
+              }
+              alt={person?.fullName || 'Name'}
               style={{
-                height: "100%",
-                width: "100%",
-                objectFit: "cover",
-                borderRadius: "100rem",
-                display: "flex",
-                justifyContent: "center",
-                alignContent: "center",
+                height: '100%',
+                width: '100%',
+                objectFit: 'cover',
+                borderRadius: '100rem',
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
               }}
             />
           </div>
           <Image
             maw={300}
-            mx="auto"
-            radius="md"
+            mx='auto'
+            radius='md'
             src={person?.funImage}
-            alt="Vector staff"
+            alt='Vector staff'
           />
         </Col>
         <Col
@@ -260,21 +286,20 @@ export function StaffPage() {
           sm={12}
           md={6}
           sx={{
-            "@media (max-width: 994px)": {
-              display: "flex",
-              justifyContent: "center",
+            '@media (max-width: 994px)': {
+              display: 'flex',
+              justifyContent: 'center',
             },
-            "@media (min-width: 994px)": {
-              display: "none",
+            '@media (min-width: 994px)': {
+              display: 'none',
             },
-          }}
-        >
+          }}>
           <Image
             maw={300}
-            mx="auto"
-            radius="md"
-            src="../src/assets/staff.png"
-            alt="Vector staff"
+            mx='auto'
+            radius='md'
+            src='/assets/staff.png'
+            alt='Vector staff'
           />
         </Col>
       </Grid>
